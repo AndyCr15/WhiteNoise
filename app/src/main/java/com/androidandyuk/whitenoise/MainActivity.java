177c11358901eda0cbe9284cc27108dbd1b67e29
Andy Crowther <andycr15@gmail.com>
2017-04-24 07:17:22 +0100

    Plays the sound clicked once

--- app/src/main/java/com/androidandyuk/whitenoise/MainActivity.java
+++ app/src/main/java/com/androidandyuk/whitenoise/MainActivity.java
@@ -1,7 +1,10 @@
 package com.androidandyuk.whitenoise;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped (View view){
+
+        // get the id in a String, of the button that has been tapped
+        int id = view.getId();
+
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        Log.i("button tapped", ourId);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.androidandyuk.whitenoise");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+        Log.i("button tapped", ourId);
+
     }
 }
