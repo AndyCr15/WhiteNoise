49b5a034291f3f2bf02910fa4593d27061957d82
Andy Crowther <andycr15@gmail.com>
2017-04-28 08:44:56 +0100

    All buttons funcitonal.  Need timer adding.
    Need to find bug that stops noise playing at times.

--- app/src/main/java/com/androidandyuk/whitenoise/MainActivity.java
+++ app/src/main/java/com/androidandyuk/whitenoise/MainActivity.java
@@ -30,7 +30,38 @@ public class MainActivity extends AppCompatActivity {
     double rainVol = 1;
     SeekBar rainVolumeControl;
 
+    int fanStreaming = 0;
+    int fanId = 0;
+    double fanVol = 1;
+    SeekBar fanVolumeControl;
 
+    int trainStreaming = 0;
+    int trainId = 0;
+    double trainVol = 1;
+    SeekBar trainVolumeControl;
+
+    int wavesStreaming = 0;
+    int wavesId = 0;
+    double wavesVol = 1;
+    SeekBar wavesVolumeControl;
+
+    int windStreaming = 0;
+    int windId = 0;
+    double windVol = 1;
+    SeekBar windVolumeControl;
+
+    int dryerStreaming = 0;
+    int dryerId = 0;
+    double dryerVol = 1;
+    SeekBar dryerVolumeControl;
+
+    int carStreaming = 0;
+    int carId = 0;
+    double carVol = 1;
+    SeekBar carVolumeControl;
+
+
+    //app starts with no noise playing
     boolean whiteNoisePlaying = false;
     boolean rainPlaying = false;
     boolean fanPlaying = false;
@@ -146,15 +177,44 @@ public class MainActivity extends AppCompatActivity {
         maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 
         whiteNoiseId = mySound.load(this, R.raw.whitenoise, 1);
-        rainId = mySound.load(this, R.raw.rain, 1);
-
         noiseVolumeControl = (SeekBar) findViewById(R.id.noiseSeekBar);
         noiseVolumeControl.setMax(maxVolume);
         noiseVolumeControl.setProgress(maxVolume);
 
+        rainId = mySound.load(this, R.raw.rain, 1);
         rainVolumeControl = (SeekBar) findViewById(R.id.rainSeekBar);
         rainVolumeControl.setMax(maxVolume);
         rainVolumeControl.setProgress(maxVolume);
+
+        fanId = mySound.load(this, R.raw.fan, 1);
+        fanVolumeControl = (SeekBar) findViewById(R.id.fanSeekBar);
+        fanVolumeControl.setMax(maxVolume);
+        fanVolumeControl.setProgress(maxVolume);
+
+        trainId = mySound.load(this, R.raw.train, 1);
+        trainVolumeControl = (SeekBar) findViewById(R.id.trainSeekBar);
+        trainVolumeControl.setMax(maxVolume);
+        trainVolumeControl.setProgress(maxVolume);
+
+        wavesId = mySound.load(this, R.raw.waves, 1);
+        wavesVolumeControl = (SeekBar) findViewById(R.id.wavesSeekBar);
+        wavesVolumeControl.setMax(maxVolume);
+        wavesVolumeControl.setProgress(maxVolume);
+
+        windId = mySound.load(this, R.raw.wind, 1);
+        windVolumeControl = (SeekBar) findViewById(R.id.windSeekBar);
+        windVolumeControl.setMax(maxVolume);
+        windVolumeControl.setProgress(maxVolume);
+
+        dryerId = mySound.load(this, R.raw.dryer, 1);
+        dryerVolumeControl = (SeekBar) findViewById(R.id.dryerSeekBar);
+        dryerVolumeControl.setMax(maxVolume);
+        dryerVolumeControl.setProgress(maxVolume);
+
+        carId = mySound.load(this, R.raw.car, 1);
+        carVolumeControl = (SeekBar) findViewById(R.id.carSeekBar);
+        carVolumeControl.setMax(maxVolume);
+        carVolumeControl.setProgress(maxVolume);
 
 
         noiseVolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -186,6 +246,132 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.i("Rain Volume", String.valueOf(progress) + " " + String.valueOf((float) rainVol));
                 mySound.setVolume(rainId, (float) rainVol, (float) rainVol);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        fanVolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double fanVol = (double) progress / (double) maxVolume;
+
+                Log.i("Fan Volume", String.valueOf(progress) + " " + String.valueOf((float) fanVol));
+                mySound.setVolume(fanId, (float) fanVol, (float) fanVol);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        trainVolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double trainVol = (double) progress / (double) maxVolume;
+
+                Log.i("Train Volume", String.valueOf(progress) + " " + String.valueOf((float) trainVol));
+                mySound.setVolume(trainId, (float) trainVol, (float) trainVol);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        wavesVolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double wavesVol = (double) progress / (double) maxVolume;
+
+                Log.i("Waves Volume", String.valueOf(progress) + " " + String.valueOf((float) wavesVol));
+                mySound.setVolume(wavesId, (float) wavesVol, (float) wavesVol);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        windVolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double windVol = (double) progress / (double) maxVolume;
+
+                Log.i("Wind Volume", String.valueOf(progress) + " " + String.valueOf((float) windVol));
+                mySound.setVolume(windId, (float) windVol, (float) windVol);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        dryerVolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double dryerVol = (double) progress / (double) maxVolume;
+
+                Log.i("Dryer Volume", String.valueOf(progress) + " " + String.valueOf((float) dryerVol));
+                mySound.setVolume(dryerId, (float) dryerVol, (float) dryerVol);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        carVolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double carVol = (double) progress / (double) maxVolume;
+
+                Log.i("Car Volume", String.valueOf(progress) + " " + String.valueOf((float) carVol));
+                mySound.setVolume(carId, (float) carVol, (float) carVol);
             }
 
             @Override
@@ -257,126 +443,169 @@ public class MainActivity extends AppCompatActivity {
     public void fanTapped(View view) {
 
         Log.i("Fan", "button tapped");
-
         if (fanPlaying) {
-            Log.i("Rain", "already playing");
-//            mpFan.pause();
+            Log.i("Fan", "already playing");
+            mySound.pause(fanStreaming);
             fanPlaying = false;
+            //change image to that showing the button is pressed
             ((ImageView) view).setImageResource(R.drawable.fan);
             isPlaying--;
+            // make seekbar invisible
+            fanVolumeControl.setVisibility(View.INVISIBLE);
         } else if (isPlaying < 3) {
+            // check not passed maximum tracks allowed
             Log.i("Fan", "start playing");
-//            mpFan = MediaPlayer.create(this, R.raw.fan);
-//            mpFan.start();
-//            mpFan.setLooping(true);
-            ((ImageView) view).setImageResource(R.drawable.fanpressed);
+            // check if the sound item hasn't been set up yet, set it up if it hasn't
+            if (fanStreaming == 0) {
+                fanStreaming = mySound.play(fanId, (float) fanVol, (float) fanVol, 1, -1, 1);
+            } else {
+                //if it's been set up, simply resume it
+                mySound.resume(fanStreaming);
+            }
             fanPlaying = true;
+            ((ImageView) view).setImageResource(R.drawable.fanpressed);
             isPlaying++;
-        }
-    }
-
-    public void windTapped(View view) {
-
-        Log.i("Wind", "button tapped");
-
-        if (windPlaying) {
-            Log.i("Rain", "already playing");
-//            mpWind.pause();
-            windPlaying = false;
-            ((ImageView) view).setImageResource(R.drawable.wind);
-            isPlaying--;
-        } else if (isPlaying < 3) {
-            Log.i("Wind", "start playing");
-//            mpWind = MediaPlayer.create(this, R.raw.wind);
-//            mpWind.start();
-//            mpWind.setLooping(true);
-            ((ImageView) view).setImageResource(R.drawable.windpressed);
-            windPlaying = true;
-            isPlaying++;
-        }
-    }
-
-    public void wavesTapped(View view) {
-
-        Log.i("Waves", "button tapped");
-
-        if (wavesPlaying) {
-            Log.i("Rain", "already playing");
-//            mpWaves.pause();
-            wavesPlaying = false;
-            ((ImageView) view).setImageResource(R.drawable.waves);
-            isPlaying--;
-        } else if (isPlaying < 3) {
-            Log.i("Waves", "start playing");
-//            mpWaves = MediaPlayer.create(this, R.raw.waves);
-//            mpWaves.start();
-//            mpWaves.setLooping(true);
-            ((ImageView) view).setImageResource(R.drawable.wavespressed);
-            wavesPlaying = true;
-            isPlaying++;
+            fanVolumeControl.setVisibility(View.VISIBLE);
         }
     }
 
     public void trainTapped(View view) {
-
         Log.i("Train", "button tapped");
-
         if (trainPlaying) {
-            Log.i("Rain", "already playing");
-//            mpTrain.pause();
+            Log.i("Train", "already playing");
+            mySound.pause(trainStreaming);
             trainPlaying = false;
+            //change image to that showing the button is pressed
             ((ImageView) view).setImageResource(R.drawable.train);
             isPlaying--;
+            // make seekbar invisible
+            trainVolumeControl.setVisibility(View.INVISIBLE);
         } else if (isPlaying < 3) {
+            // check not passed maximum tracks allowed
             Log.i("Train", "start playing");
-//            mpTrain = MediaPlayer.create(this, R.raw.train);
-//            mpTrain.start();
-//            mpTrain.setLooping(true);
-            ((ImageView) view).setImageResource(R.drawable.trainpressed);
+            // check if the sound item hasn't been set up yet, set it up if it hasn't
+            if (trainStreaming == 0) {
+                trainStreaming = mySound.play(trainId, (float) trainVol, (float) trainVol, 1, -1, 1);
+            } else {
+                //if it's been set up, simply resume it
+                mySound.resume(trainStreaming);
+            }
             trainPlaying = true;
+            ((ImageView) view).setImageResource(R.drawable.trainpressed);
             isPlaying++;
+            trainVolumeControl.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void wavesTapped(View view) {
+        Log.i("Waves", "button tapped");
+        if (wavesPlaying) {
+            Log.i("Waves", "already playing");
+            mySound.pause(wavesStreaming);
+            wavesPlaying = false;
+            //change image to that showing the button is pressed
+            ((ImageView) view).setImageResource(R.drawable.waves);
+            isPlaying--;
+            // make seekbar invisible
+            wavesVolumeControl.setVisibility(View.INVISIBLE);
+        } else if (isPlaying < 3) {
+            // check not passed maximum tracks allowed
+            Log.i("Waves", "start playing");
+            // check if the sound item hasn't been set up yet, set it up if it hasn't
+            if (wavesStreaming == 0) {
+                wavesStreaming = mySound.play(wavesId, (float) wavesVol, (float) wavesVol, 1, -1, 1);
+            } else {
+                //if it's been set up, simply resume it
+                mySound.resume(wavesStreaming);
+            }
+            wavesPlaying = true;
+            ((ImageView) view).setImageResource(R.drawable.wavespressed);
+            isPlaying++;
+            wavesVolumeControl.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void windTapped(View view) {
+        Log.i("Wind", "button tapped");
+        if (windPlaying) {
+            Log.i("Wind", "already playing");
+            mySound.pause(windStreaming);
+            windPlaying = false;
+            //change image to that showing the button is pressed
+            ((ImageView) view).setImageResource(R.drawable.wind);
+            isPlaying--;
+            // make seekbar invisible
+            windVolumeControl.setVisibility(View.INVISIBLE);
+        } else if (isPlaying < 3) {
+            // check not passed maximum tracks allowed
+            Log.i("Wind", "start playing");
+            // check if the sound item hasn't been set up yet, set it up if it hasn't
+            if (windStreaming == 0) {
+                windStreaming = mySound.play(windId, (float) windVol, (float) windVol, 1, -1, 1);
+            } else {
+                //if it's been set up, simply resume it
+                mySound.resume(windStreaming);
+            }
+            windPlaying = true;
+            ((ImageView) view).setImageResource(R.drawable.windpressed);
+            isPlaying++;
+            windVolumeControl.setVisibility(View.VISIBLE);
         }
     }
 
     public void dryerTapped(View view) {
-
         Log.i("Dryer", "button tapped");
-
         if (dryerPlaying) {
-            Log.i("Rain", "already playing");
-//            mpDryer.pause();
+            Log.i("Dryer", "already playing");
+            mySound.pause(dryerStreaming);
             dryerPlaying = false;
+            //change image to that showing the button is pressed
             ((ImageView) view).setImageResource(R.drawable.dryer);
             isPlaying--;
+            // make seekbar invisible
+            dryerVolumeControl.setVisibility(View.INVISIBLE);
         } else if (isPlaying < 3) {
+            // check not passed maximum tracks allowed
             Log.i("Dryer", "start playing");
-//            mpDryer = MediaPlayer.create(this, R.raw.dryer);
-//            mpDryer.start();
-//            mpDryer.setLooping(true);
-            ((ImageView) view).setImageResource(R.drawable.dryerpressed);
+            // check if the sound item hasn't been set up yet, set it up if it hasn't
+            if (dryerStreaming == 0) {
+                dryerStreaming = mySound.play(dryerId, (float) dryerVol, (float) dryerVol, 1, -1, 1);
+            } else {
+                //if it's been set up, simply resume it
+                mySound.resume(dryerStreaming);
+            }
             dryerPlaying = true;
+            ((ImageView) view).setImageResource(R.drawable.dryerpressed);
             isPlaying++;
+            dryerVolumeControl.setVisibility(View.VISIBLE);
         }
     }
 
     public void carTapped(View view) {
-
-        Log.i("Car", "button tapped");
-
+        Log.i("Dryer", "button tapped");
         if (carPlaying) {
-            Log.i("Rain", "already playing");
-//            mpCar.pause();
+            Log.i("Dryer", "already playing");
+            mySound.pause(carStreaming);
             carPlaying = false;
+            //change image to that showing the button is pressed
             ((ImageView) view).setImageResource(R.drawable.car);
             isPlaying--;
+            // make seekbar invisible
+            carVolumeControl.setVisibility(View.INVISIBLE);
         } else if (isPlaying < 3) {
-            Log.i("Car", "start playing");
-//            mpCar = MediaPlayer.create(this, R.raw.car);
-//            mpCar.start();
-//            mpCar.setLooping(true);
-            ((ImageView) view).setImageResource(R.drawable.carpressed);
+            // check not passed maximum tracks allowed
+            Log.i("Dryer", "start playing");
+            // check if the sound item hasn't been set up yet, set it up if it hasn't
+            if (carStreaming == 0) {
+                carStreaming = mySound.play(carId, (float) carVol, (float) carVol, 1, -1, 1);
+            } else {
+                //if it's been set up, simply resume it
+                mySound.resume(carStreaming);
+            }
             carPlaying = true;
+            ((ImageView) view).setImageResource(R.drawable.carpressed);
             isPlaying++;
+            carVolumeControl.setVisibility(View.VISIBLE);
         }
     }
 }
